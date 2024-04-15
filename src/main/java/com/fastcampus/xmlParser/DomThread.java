8602a79b8fe1e2565e9013ff983e5a42afd65417
JSH1990 <tmdgus21@naver.com>
2024-04-15 14:27:17 +0900

    first.

---
+++ src/main/java/com/fastcampus/xmlParser/DomThread.java
@@ -0,0 +1,8 @@
+package com.fastcampus.xmlParser;
+
+public class DomThread extends Thread {
+    //다운로드 받을 문자열을 저장할 변수
+    private String xml;
+    //파싱할 결과를 저장할 리스트 - 몇개인지 알수가 없기때문에, 배열이 아니고 ArrayList로 만들어야함
+
+}
